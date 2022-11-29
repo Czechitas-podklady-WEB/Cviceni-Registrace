96d401a0f879c64b23a25826175f6101b53324fd
Filip Chalupa <chalupa.filip@gmail.com>
2022-11-29 14:40:44 +0100

    Odebrání use strict

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-'use strict';
-
 const users = [
-  'petr',
-  'jana-34',
-  'husky16',
-  '66beruska',
-  'thegodofU',
-  'rene_gade',
-  'tommy',
-  'luciluci',
-  'tanko',
-  'poltergeist',
-  'hlava-havla',
-  'telo-ramba'
-];
-
+	'petr',
+	'jana-34',
+	'husky16',
+	'66beruska',
+	'thegodofU',
+	'rene_gade',
+	'tommy',
+	'luciluci',
+	'tanko',
+	'poltergeist',
+	'hlava-havla',
+	'telo-ramba',
+]
