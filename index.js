093f899c21ddef1d3ad18f9367be4320796ede25
Filip Chalupa <chalupa.filip@gmail.com>
2022-11-29 15:04:39 +0100

    Řešení

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ const users = [
 	'hlava-havla',
 	'telo-ramba',
 ]
+
+document.querySelector('.reg-form').addEventListener('submit', (event) => {
+	event.preventDefault()
+	const username = document.querySelector('#username')
+	const pass1 = document.querySelector('#pass1')
+	const error = document.querySelector('.reg-form__error')
+
+	if (users.includes(username.value)) {
+		error.textContent = `Zadané jméno „${username.value}“ je již zabráno.`
+		return
+	}
+	if (
+		pass1.value.length < 10 ||
+		(pass1.value.includes('-') === false &&
+			pass1.value.includes('_') === false &&
+			pass1.value.includes(':') === false)
+	) {
+		error.textContent = 'Zadané heslo není dostatečně bezpečné.'
+		return
+	}
+	error.textContent = ''
+	alert('Uživatel úspěšně zaregistrovaný.')
+})
